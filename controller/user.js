01b632eeaa960e79ce60adb81f9cf829a2b6276a
Marcos Taus <mactaus@gmail.com>
2021-08-26 11:12:21 -0300

    Improve CRUD endpoints with db integration

--- controller/user.js
+++ controller/user.js
@@ -1,28 +1,54 @@
+const { request, response } = require('express');
 const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
-const getUser = (req, res) => {
-    const {q, name = "No name", page} = req.query;
+const getUser = async (req = request, res = response) => {
+    //const {q, name = "No name", page} = req.query;
+    const { limit = 5, from = 0 } = req.query;
+    const queryActiveUsers = { state: true };
     
+    const [total, users] = await Promise.all([
+        User.countDocuments(queryActiveUsers),
+        User.find(queryActiveUsers)
+        .skip(Number(from))
+        .limit(Number(limit))
+    ]);
+    /*
     res.json({
         message: 'get API - controller',
         q,
         name,
         page
     });
+    */
+   res.json({total, users});
 };
 
-const putUser = (req, res) => {
+const putUser = async (req = request, res = response) => {
     const { id } = req.params;
+    const { password, google, ...params} = req.body;
+
+    if (password) {
+        // hash password
+        const salt = bcryptjs.genSaltSync();
+        params.password = bcryptjs.hashSync(password, salt);
+    }
+
+    const userUpdated = await User.findByIdAndUpdate(id, params);
 
     res.json({
         message: 'put API - controller',
-        id
+        user: userUpdated
     });
 };
 
-const postUser = async (req, res) => {
-    const body = req.body;
-    const user = new User(body);
+const postUser = async (req = request, res = response) => {
+    const { name, email, password, role } = req.body;
+    const user = new User({ name, email, password, role });
+
+    // hash password
+    const salt = bcryptjs.genSaltSync();
+    user.password = bcryptjs.hashSync(password, salt);
 
     await user.save();
 
@@ -32,9 +58,16 @@ const postUser = async (req, res) => {
     });
 };
 
-const deleteUser = (req, res) => {
+const deleteUser = async(req = request, res = response) => {
+    const { id } = req.params;
+
+    // hard delete
+    // const userDeleted = await User.findByIdAndDelete(id);
+    //soft delete
+    const userDeleted = await User.findByIdAndUpdate(id, { state: false });
     res.json({
-        message: 'delete API - controller'
+        message: 'delete API - controller',
+        user: userDeleted
     });
 };
 
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,33 @@
 const { Router } = require('express');
 const { getUser, postUser, putUser, deleteUser } = require('../controller/user');
+const { check } = require('express-validator');
+const { fieldValidator } = require('../middlewares/field-validator');
+const { isRoleValid, existsEmail, existsUserById } = require('../utils/db-validators')
 
 const router = Router();
 
 router.get('/', getUser);
 
-router.put('/:id', putUser);
+router.put('/:id', [
+    check('id', 'Not a valid Mongo ID').isMongoId(),
+    check('id').custom(existsUserById),
+    fieldValidator
+], putUser);
 
-router.post('/', postUser);
+router.post('/', [
+    check('name', 'Name is not present').not().isEmpty(),
+    check('password', 'Password must have more than 6').isLength({ min: 6 }),
+    check('email', 'Email is not valid').isEmail(),
+    check('email').custom( existsEmail ),
+    //check('role', 'Role is not valid').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom( isRoleValid ),
+    fieldValidator
+], postUser);
 
-router.delete('/', deleteUser);
+router.delete('/:id', [
+    check('id', 'Not a valid Mongo ID').isMongoId(),
+    check('id').custom(existsUserById),
+    fieldValidator
+], deleteUser);
 
 module.exports =  router
